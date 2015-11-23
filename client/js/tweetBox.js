a93a03f34ac0e437685d4e5b6effcf73e1903781
Tom Martin <tom@tpgmartin.com>
2015-11-23 23:06:38 +0000

    Remove autopublish, insecure, restructure project

---
+++ client/js/tweetBox.js
@@ -0,0 +1,38 @@
+// push session variable changes to HTML
+Template.tweetBox.helpers({
+  charCount: function () {
+    return 140 - session.get('numChars');
+  },
+
+  charClass: function () {
+    if (Session.get('numChars') > 140) {
+      return 'errCharCount';
+    } else {
+      return 'charCount';
+    }
+  },
+
+  disableButton: function () {
+    if (Session.get('numChars') <= 0 || Session.get('numChars') > 140 || !Meteor.user()) {
+      return 'disabled';
+    }
+  }
+});
+
+// update character count on typing
+Template.tweetBox.events({
+  'input #tweetText': function () {
+    Session.set('numChars', $('#tweetText').val().length);
+  },
+
+  'click button': function () {
+    var tweet = $('#tweetText').val();
+    $('#tweetText').val('');
+    Session.set('numChars', 0);
+    Tweets.insert({message: tweet, user: Meteor.user().username});
+  }
+});
+
+Template.tweetBox.onRendered(function () {
+  Session.set('numChars', 0);
+});
